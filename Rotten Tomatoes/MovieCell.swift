2e7d4c6b766fc8daa46c52adb187f845aa33aace
Janki Chauhan <janki.chauhan@gmail.com>
2015-05-07 12:42:10 -0700

    Custom cells

---
+++ Rotten Tomatoes/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Rotten Tomatoes
+//
+//  Created by Jaimin Shah on 5/7/15.
+//  Copyright (c) 2015 Janki Chauhan. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
